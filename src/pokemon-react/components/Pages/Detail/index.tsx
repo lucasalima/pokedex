d2963cbeef87a67ffec128f10a220967b55009cc
lucas_alima <lucas_alima@outlook.com>
2020-06-12 18:17:15 -0300

    Commit

--- src/pokemon-react/components/Pages/Detail/index.tsx
+++ src/pokemon-react/components/Pages/Detail/index.tsx
@@ -40,49 +40,51 @@ const Detail = () => {
             <NotFound />
           ) : (
             <div key="pokemon-cell" className="pokemon-cell">
-              <div className="image">
-                <img
-                  src={`${spriteUrl}/${pokemon.id}.png`}
-                  alt={pokemon.name}
-                />
-              </div>
+              <div className="box">
+                <div className="image">
+                  <img
+                    src={`${spriteUrl}/${pokemon.id}.png`}
+                    alt={pokemon.name}
+                  />
+                </div>
 
-              <div className="info">
-                <div className="title">Pokémon profile</div>
-                <ul className="profile">
-                  <li>
-                    <span className="name">ID:</span>
-                    <span className="detail">{pokemon.id}</span>
-                  </li>
-                  <li>
-                    <span className="name">Name:</span>
-                    <span className="detail">{pokemon.name}</span>
-                  </li>
-                  <li>
-                    <span className="name">Height:</span>
-                    <span className="detail">{pokemon.height}</span>
-                  </li>
-                  <li>
-                    <span className="name">Weight:</span>
-                    <span className="detail">{pokemon.weight}</span>
-                  </li>
-                </ul>
+                <div className="info">
+                  <div className="title">Pokémon profile</div>
+                  <ul className="profile">
+                    <li>
+                      <span className="name">ID:</span>
+                      <span className="detail">{pokemon.id}</span>
+                    </li>
+                    <li>
+                      <span className="name">Name:</span>
+                      <span className="detail">{pokemon.name}</span>
+                    </li>
+                    <li>
+                      <span className="name">Height:</span>
+                      <span className="detail">{pokemon.height}</span>
+                    </li>
+                    <li>
+                      <span className="name">Weight:</span>
+                      <span className="detail">{pokemon.weight}</span>
+                    </li>
+                  </ul>
 
-                <div className="title">Other sprites</div>
-                <ul className="sprites">
-                  <li>
-                    <img
-                      src={pokemon.sprites.front_default}
-                      alt={pokemon.name}
-                    />
-                  </li>
-                  <li>
-                    <img
-                      src={pokemon.sprites.back_default}
-                      alt={pokemon.name}
-                    />
-                  </li>
-                </ul>
+                  <div className="title">Other sprites</div>
+                  <ul className="sprites">
+                    <li>
+                      <img
+                        src={pokemon.sprites.front_default}
+                        alt={pokemon.name}
+                      />
+                    </li>
+                    <li>
+                      <img
+                        src={pokemon.sprites.back_default}
+                        alt={pokemon.name}
+                      />
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           ),
